d5f45eac4d6c2f5fce41929082ee2d5e1a66beb1
Austinisperson <144163050+Austinisperson@users.noreply.github.com>
2023-09-18 21:42:59 -0500

    ANIMATION + MOVEMENT DONE

--- Perc30hansa/Assets/PlayerMovement.cs
+++ Perc30hansa/Assets/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
 
+    public Animator animator;
+
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
@@ -15,6 +17,8 @@ public class PlayerMovement : MonoBehaviour {
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
